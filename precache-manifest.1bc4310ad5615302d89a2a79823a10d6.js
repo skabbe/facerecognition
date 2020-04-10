807f9425ccfb329a93132632275bf83900ada5fa
skabbe <lothloriell@yahoo.co.uk>
2020-04-10 17:26:49 +0200

    Updates

---
+++ precache-manifest.1bc4310ad5615302d89a2a79823a10d6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "251a608eebe7f8d7a62f7aac1be03ac3",
+    "url": "/facerecognition/index.html"
+  },
+  {
+    "revision": "8aa7f672081ddaa690c7",
+    "url": "/facerecognition/static/css/2.3b6f1bda.chunk.css"
+  },
+  {
+    "revision": "2e9b0229eecc2aeb2506",
+    "url": "/facerecognition/static/css/main.ce94daed.chunk.css"
+  },
+  {
+    "revision": "8aa7f672081ddaa690c7",
+    "url": "/facerecognition/static/js/2.960c9f3b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/facerecognition/static/js/2.960c9f3b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e9b0229eecc2aeb2506",
+    "url": "/facerecognition/static/js/main.174bd30c.chunk.js"
+  },
+  {
+    "revision": "1ac15efaf6440087ebf6",
+    "url": "/facerecognition/static/js/runtime-main.1e11fefe.js"
+  }
+]);
